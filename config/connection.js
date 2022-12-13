c659f6631af8abdbdd75fa5c0779b2047fcb91eb
Tiffany Marko <tiffanyjmarko@gmail.com>
2022-12-12 16:17:44 -0800

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
     logging: false
   }
 );
